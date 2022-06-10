5baabdc3efa37343dfbfd2f27e874f775f140203
jseepaul1 <seepauljulie@gmail.com>
2022-06-09 21:09:50 -0400

    made changes to todolist

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -1,47 +1,60 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 const TodoList = ({ todoList, setTodoList }) => {
-    
-    const handleTodoSubmit = (e) => {
-        // this prevents the page from reloading
-        e.preventDefault();
-        
-    }
+  const [item, setItem] = useState({
+    // the inital value will be set to an empty string
+    todoTask: "",
+    // this will be set to false until checkbox is checked
+    isTaskComplete: false,
+  });
 
-    let item = {
-        // the inital value will be set to an empty string
-        todoTask: "",
-        // this will be set to false until checkbox is checked
-        isTaskComplete: false
-    };
+  const handleTodoSubmit = (e) => {
+    // this prevents the page from reloading
+    e.preventDefault();
 
-    const ItemOnChange = e => {
-        item.todoTask = e.target.value;
-    }
+    // ... is a spread operator which will create a new array with the current todos plus the new todo added
+    setTodoList([...todoList, item]);
 
-    const handleTodo = e => {
-        // ... is a spread operator which will create a new array with the current todos plus the new todo added
-        setTodoList([...todoList, item]);
-    }
+    setItem({
+      // the inital value will be set to an empty string
+      todoTask: "",
+      // this will be set to false until checkbox is checked
+      isTaskComplete: false,
+    });
+  };
 
-    return (
-        <Container>
-        <div>
-            <h1>Todo List</h1>
-            <form className= "todo-form" onSubmit={e => {handleTodoSubmit(e)}}>
-            <input className="form-control"
-                placeholder= "Enter a todo task"
-                type="text"
-                name="item"
-                onChange={ItemOnChange} />
-            <Button variant="primary" className="btn" onClick={handleTodo}>Add Todo</Button>
-            </form>
-        </div>
-        </Container>
-    )
-}
+  const ItemOnChange = (e) => {
+    setItem({
+      ...item,
+      todoTask: e.target.value,
+    });
+  };
+
+  return (
+    <Container>
+      <div>
+        <h1>Todo List</h1>
+        <form
+          className="todo-form"
+          onSubmit={handleTodoSubmit}
+        >
+          <input
+            className="form-control"
+            placeholder="Enter a todo task"
+            type="text"
+            name="item"
+            value={item.todoTask}
+            onChange={ItemOnChange}
+          />
+          <Button variant="primary" className="btn" type="submit">
+            Add Todo
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+};
 
 export default TodoList;
